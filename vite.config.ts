fb256dbca40640ed65b577c6c9893aea1c0b1b7f
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2026-06-14 14:56:02 +0300

    Update vite.config.ts
    git commit -m "fix: add base path to vite config for github pages deployment"

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/moldova-map/', // Добавляем базовый путь, чтобы GitHub Pages понимал, откуда брать JS и CSS
+})
